4a247bbab44c9a4be6954a364c86fe881313e279
Joseph Cumbo <joe+git@jcumbo.com>
2016-07-18 03:56:43 -0700

    Add a simple event bus

---
+++ src/main/java/com/gmail/mooman219/pokemongo/event/EventHandlerMethod.java
@@ -0,0 +1,27 @@
+package com.gmail.mooman219.pokemongo.event;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class EventHandlerMethod {
+
+    private final Object listener;
+    private final Method method;
+
+    public void invoke(Object event) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        method.invoke(listener, event);
+    }
+
+    public EventHandlerMethod(Object listener, Method method) {
+        this.listener = listener;
+        this.method = method;
+    }
+
+    public Object getListener() {
+        return listener;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+}
---
+++ src/main/java/com/gmail/mooman219/pokemongo/event/EventPriority.java
@@ -0,0 +1,17 @@
+package com.gmail.mooman219.pokemongo.event;
+
+/**
+ * Importance of the {@link EventHandler}. When executing an Event, the handlers
+ * are called in order of their Priority.
+ */
+public final class EventPriority {
+
+    public static final byte LOWEST = -64;
+    public static final byte LOW = -32;
+    public static final byte NORMAL = 0;
+    public static final byte HIGH = 32;
+    public static final byte HIGHEST = 64;
+
+    private EventPriority() {
+    }
+}
